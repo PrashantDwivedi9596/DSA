bdbec77d1c5b656d758fe1352ce119af1de782c1
Prashant Dwivedi <codertruf@gmail.com>
2026-08-15 17:45:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (90.22%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int case1 = LinearRob(nums,0, n-2);
+        int case2 = LinearRob(nums,1, n-1);
+        return Math.max(case1, case2);
+    }
+
+    public int LinearRob(int[] nums, int start, int end){
+        int prev1=0;
+        int prev2=0;
+        for(int i = start; i<=end;i++){
+            int curr = Math.max(prev1, prev2+nums[i]);
+            prev2= prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+}
